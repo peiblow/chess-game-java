b2ca2de3114f04bd4697c4479e69a7ce9c84230e
Pablo Fernandes <veooow@gmail.com>
2024-05-12 00:57:12 -0300

    feat: gameController + print board

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import gameController.ChessMatch;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
---
+++ src/gameController/Color.java
@@ -0,0 +1,6 @@
+package gameController;
+
+public enum Color {
+    BLACK,
+    WHITE
+}
